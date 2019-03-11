151c641a6190a6d1d9b1a18496da5220e0bde96f
1027 <1027@hb1009>
2019-03-11 18:17:21 +0900

    2019-03-11 18:17

--- bit_trade/src/main/java/com/bit_trade/web/controller/CustomerController.java
+++ bit_trade/src/main/java/com/bit_trade/web/controller/CustomerController.java
@@ -1,19 +1,20 @@
 package com.bit_trade.web.controller;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.bit_trade.web.domain.CustomerDTO;
-import com.bit_trade.web.proxy.ContextProxy;
 import com.bit_trade.web.service.CustomerService;
 
 @SessionAttributes({"cust"})
@@ -23,34 +24,42 @@ public class CustomerController {
 	static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
 	@Autowired CustomerDTO customer;
 	@Autowired CustomerService customerService;
-	@Autowired HttpSession session;
-	@Autowired ContextProxy pxy;
 	
 	@RequestMapping(value="/signin",method=RequestMethod.POST)
-	public String signin(@ModelAttribute CustomerDTO param,HttpSession session ) {
+	public String signin(@ModelAttribute CustomerDTO param,Model session ) { // 지역변수 Model (HttpSession이 값을 오래 물고? 있음)
 		logger.info("\n --------- customerController {} !! ----------","signin");
 		System.out.println("로그인 전 결과 : "+customer);
 
 		customer = customerService.retrieveCustomer(param);
 		System.out.println("로그인 후 결과 : "+customer);
-		session.setAttribute("cust", customer);
-		if(customer != null) session.setAttribute("cust", customer);
+		session.addAttribute("cust", customer);
+		if(customer != null) session.addAttribute("cust", customer);
 		return (customer != null) ? 
 				"public:customer/detail.tiles"
 					:
 				"public:home/main.tiles";
 	}
-	@RequestMapping(value="/update", method=RequestMethod.POST)
-	public String update(@ModelAttribute CustomerDTO param) {
-		logger.info("\n --------- customerController {} !! ----------","update");
-		System.out.println("수정 전 : "+ session.getAttribute("cust"));
-		customerService.modifyCustomer(param);
-		session.invalidate(); // 기존에 담아둔거 정리
-		pxy.setContext();
-		session.setAttribute("cust", customerService.retrieveCustomer(param));
-		System.out.println("수정 후 결과 : "+session.getAttribute("cust"));
-		
+	
+	@RequestMapping(value="/detail")
+	public String detail(@RequestParam("customerID")String customerID, Model model) { 
+		logger.info("\n --------- customerController {} !! ----------","detail");
+		customer.setCustomerID(customerID);
+		customer = customerService.retrieveCustomer(customer);
+		model.addAttribute("cust",customer);
 		return "public:customer/detail.tiles";
+		
+	}
+	@RequestMapping(value="/update", method=RequestMethod.POST)
+	public String update(@ModelAttribute CustomerDTO param,Model session, RedirectAttributes red) {
+		logger.info("\n --------- customerController {} !! ----------","update");
+		System.out.println("수정 전 : "+ session.addAttribute("cust"));
+		customerService.modifyCustomer(param);
+		customer = customerService.retrieveCustomer(param);
+		session.addAttribute("cust",customer);
+		red.addAttribute("customerID",param.getCustomerID());
+		System.out.println("수정 후 결과 : "+session.addAttribute("cust"));
+		
+		return "redirect:/customer/detail"; //내장 
 		
 	}
 }
